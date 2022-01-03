2ebd52e1624398f711bdefa94381a7d347b34cc4
Dreezy <bankoleidris@gmail.com>
2022-01-03 23:52:02 +0100

    constant file

---
+++ constants/images.js
@@ -0,0 +1,4 @@
+const Avatar = require("../assets/images/Avatar.png");
+const Avatar2 = require("../assets/images/Avatar2.png");
+const Bride = require("../assets/images/bride.png");
+const discover = require("../assets/images/discover.png");
