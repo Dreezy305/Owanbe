498584150780c62e71c5b2ad3e29af54f10bfbbd
Dreezy <bankoleidris@gmail.com>
2022-02-25 09:49:16 +0000

    loading icons

---
+++ constants/color_theme_styles.js
@@ -0,0 +1,32 @@
+import { Dimensions } from "react-native";
+
+export const COLORS = {
+  red: "#E20000",
+  purple: "#19235B",
+};
+
+export const FONTS = {
+    fontStyles = { normal: "normal", italic: "italic" },
+    fontWeights = {fontNormal: "normal",
+  fontBold: "bold",
+  font100: "100",
+  font200: "200",
+  font300: "300",
+  font400: "400",
+  font500: "500",
+  font600: "600",
+  font700: "700",
+  font800: "800",
+  font900: "900"}
+};
+
+export const textAlignments = ["auto", "left", "right", "center", "justify"];
+export const textDecorationLines = [
+  "none",
+  "underline",
+  "line-through",
+  "underline line-through"
+];
+export const textDecorationStyles = ["solid", "double", "dotted", "dashed"];
+export const textTransformations = ["none", "uppercase", "lowercase", "capitalize"];
+export const textAlignmentsVertical = ["auto", "top", "bottom", "center"];
