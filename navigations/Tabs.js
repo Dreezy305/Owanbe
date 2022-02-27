8c659272c9fcbe5db010e3573624b82af7ef5ac0
Dreezy <bankoleidris@gmail.com>
2022-02-27 12:45:24 +0000

    tab bar

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/home";
+import OrganizeScreen from "../screens/organize";
+import ChatScreen from "../screens/chat";
+import MenuScreen from "../screens/menu";
+
+const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
+    <Tab.Navigator tabBar={}>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="OrganizeScreen" component={OrganizeScreen} />
+      <Tab.Screen name="ChatScreen" component={ChatScreen} />
+      <Tab.Screen name="MenuScreen" component={MenuScreen} />
+    </Tab.Navigator>
   );
 }
