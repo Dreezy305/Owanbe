083ed9ebafc98190ab8d9c59882cc8e14396d7a2
Dreezy <bankoleidris@gmail.com>
2022-02-27 13:11:11 +0000

    tab bar

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -5,6 +5,12 @@ import HomeScreen from "../screens/home";
 import OrganizeScreen from "../screens/organize";
 import ChatScreen from "../screens/chat";
 import MenuScreen from "../screens/menu";
+import {
+  COLORS,
+  FONTS,
+  FontWeights,
+  FontStyles,
+} from "../constants/color_theme_styles";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,22 +20,66 @@ export default function Tabs() {
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontFamily: "Avenir_Roman",
+            fontWeight: FontWeights.fw800,
+            color: COLORS.purple,
+            fontSize: 12,
+            lineHeight: 12,
+            letterSpacing: 0.12,
+            fontStle: FontStyles.normal,
+          },
+        }}
       />
       <Tab.Screen
         name="Organize"
         component={OrganizeScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontFamily: "Avenir_Roman",
+            fontWeight: FontWeights.fw800,
+            fontStle: FontStyles.normal,
+            color: COLORS.purple,
+            fontSize: 12,
+            lineHeight: 12,
+            letterSpacing: 0.12,
+          },
+        }}
       />
       <Tab.Screen
         name="Chat"
         component={ChatScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontFamily: "Avenir_Roman",
+            fontWeight: FontWeights.fw800,
+            fontStle: FontStyles.normal,
+            color: COLORS.purple,
+            fontSize: 12,
+            lineHeight: 12,
+            letterSpacing: 0.12,
+          },
+        }}
       />
       <Tab.Screen
         name="Menu"
         component={MenuScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontFamily: "Avenir_Roman",
+            fontWeight: FontWeights.fw800,
+            fontStle: FontStyles.normal,
+            color: COLORS.purple,
+            fontSize: 12,
+            lineHeight: 12,
+            letterSpacing: 0.12,
+          },
+        }}
       />
     </Tab.Navigator>
   );
