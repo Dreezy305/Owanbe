de66c187e36539648a224a02c1575cb9bc1aebd8
Dreezy <bankoleidris@gmail.com>
2022-03-05 22:27:57 +0000

    flatlist , single owanbe

--- screens/SingleOwanbe.js
+++ screens/SingleOwanbe.js
@@ -18,10 +18,42 @@ import {
 import { images, icons } from "../constants";
 
 export default function SingleOwanbe() {
+  const renderGallery = () => {
+    return (
+      <View style={styles.gallery}>
+        <Image
+          source={images.bride}
+          resizeMethod="scale"
+          resizeMode="contain"
+        />
+        <Image
+          source={images.invitation}
+          resizeMethod="scale"
+          resizeMode="contain"
+          style={{ borderRadius: 10, marginVertical: 10 }}
+        />
+        <Image
+          source={images.party_dancer}
+          resizeMethod="scale"
+          resizeMode="contain"
+          style={{ borderRadius: 10, marginVertical: 10 }}
+        />
+        <Image
+          source={images.bride}
+          resizeMethod="scale"
+          resizeMode="contain"
+          style={{ marginVertical: 10 }}
+        />
+      </View>
+    );
+  };
+
+  const renderWeddingDetails = () => {
+    return <View></View>;
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>SingleOwanbe</Text>
-    </SafeAreaView>
+    <SafeAreaView style={styles.container}>{renderGallery()}</SafeAreaView>
   );
 }
 
@@ -34,5 +66,14 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingHorizontal: 20,
     paddingTop: 30,
+  },
+
+  // GALLERY
+  gallery: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    alignItems: TextAlignments.text_center,
   },
 });
