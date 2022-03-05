ea141857bd843d46fc403b2b4f26da20fbb182ae
Dreezy <bankoleidris@gmail.com>
2022-03-05 21:57:11 +0000

    flatlist

--- screens/SingleOwanbe.js
+++ screens/SingleOwanbe.js
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import {
+  TextAlignments,
+  COLORS,
+  FontStyles,
+  FontWeights,
+  SIZES,
+} from "../constants/color_theme_styles";
+import { images, icons } from "../constants";
 
 export default function SingleOwanbe() {
   return (
