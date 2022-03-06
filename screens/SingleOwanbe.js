badd567d96f304a42e0a3590c122040cdc0829a4
Dreezy <bankoleidris@gmail.com>
2022-03-06 00:16:43 +0000

    tag and willing

--- screens/SingleOwanbe.js
+++ screens/SingleOwanbe.js
@@ -86,6 +86,7 @@ export default function SingleOwanbe({ navigation }) {
                     padding: 20,
                     textAlign: "center",
                   }}
+                  onPress={() => navigation.navigate("SearchPartner")}
                 >
                   <Text
                     style={{
