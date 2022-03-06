01442538a88e495bfaf46bc9321632c2f76eb052
Dreezy <bankoleidris@gmail.com>
2022-03-06 23:49:55 +0000

    message input

--- components /MessageComponents/MessageInput.js
+++ components /MessageComponents/MessageInput.js
@@ -1,5 +1,11 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState } from "react";
 import { images, icons } from "../../constants";
 import {
   COLORS,
@@ -11,9 +17,33 @@ import {
 } from "../../constants/color_theme_styles";
 
 export default function MessageInput() {
+  const [msg, setMsg] = useState("");
+
   return (
-    <View>
-      <Text>MessageInput</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TextInput
+        placeholder="Type here"
+        style={{ ...styles.TextInput }}
+        placeholderTextColor={COLORS.purple}
+        selectTextOnFocus
+        allowFontScaling
+        autoCapitalize="words"
+      />
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  TextInput: {
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: COLORS.red,
+    padding: 15,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    backgroundColor: "rgba(220, 220, 220, 0.25)",
+    fontFamily: "Avenir_Roman",
+  },
+});
