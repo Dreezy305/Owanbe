67e00ad95c073a03c2a9a747650383e315fb894e
Dreezy <bankoleidris@gmail.com>
2022-03-06 14:15:41 +0000

    search owanbe

--- screens/SearchPartner.js
+++ screens/SearchPartner.js
@@ -20,18 +20,32 @@ import { images, icons } from "../constants";
 
 export default function SearchPartner() {
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
       <ImageBackground
         source={images.party}
-        resizeMethod="scale"
-        resizeMode="contain"
-      />
-    </SafeAreaView>
+        style={styles.image}
+        resizeMode="cover"
+      >
+        <Text>Searching for partners</Text>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: SIZES.width,
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
