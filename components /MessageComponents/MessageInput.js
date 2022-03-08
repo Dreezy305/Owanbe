a1438ec775a7dd0cff7f76ae6f87813d1919c23a
Dreezy <bankoleidris@gmail.com>
2022-03-08 00:09:18 +0000

    chat UI

--- components /MessageComponents/MessageInput.js
+++ components /MessageComponents/MessageInput.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React, { useState, useEffect, useCallback } from "react";
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, Send } from "react-native-gifted-chat";
+import { FontAwesome } from "@expo/vector-icons";
 import { images, icons } from "../../constants";
 import {
   COLORS,
@@ -77,19 +78,47 @@ export default function MessageInput() {
             fontFamily: "Avenir_Roman",
             fontWeight: FontWeights.fw400,
             fontSize: 12,
+            lineHeight: 14,
+            letterSpacing: 1,
           },
           left: {
             color: COLORS.white,
             fontFamily: "Avenir_Roman",
             fontWeight: FontWeights.fw400,
             fontSize: 12,
+            lineHeight: 14,
+            letterSpacing: 1,
           },
         }}
       />
     );
   };
 
-  const renderSend = (props) => {};
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View
+          style={{
+            backgroundColor: COLORS.red,
+            borderRadius: 10,
+            padding: 8,
+            marginBottom: 2,
+            marginRight: 2,
+          }}
+        >
+          <Image
+            source={icons.plane}
+            resizeMode="contain"
+            resizeMethod="scale"
+          />
+        </View>
+      </Send>
+    );
+  };
+
+  const scrollToBottomComponent = () => {
+    return <FontAwesome name="angle-down" size={24} color={COLORS.purple} />;
+  };
 
   return (
     <GiftedChat
@@ -106,6 +135,23 @@ export default function MessageInput() {
       showUserAvatar
       renderBubble={renderBubble}
       renderSend={renderSend}
+      scrollToBottom
+      textInputStyle={{
+        fontFamily: "Avenir_Roman",
+        color: COLORS.purple,
+        fontSize: 14,
+        lineHeight: 17,
+        letterSpacing: 1,
+        borderRadius: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderWidth: 1,
+        marginRight: 2,
+        marginTop: 2,
+        paddingLeft: 10,
+        borderColor: COLORS.red,
+      }}
+      scrollToBottomComponent={scrollToBottomComponent}
     />
   );
 }
