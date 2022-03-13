02e96057c995fe5d40acf96d4cb0d50fa9d0cce6
Dreezy <bankoleidris@gmail.com>
2022-03-13 21:28:31 +0000

    auth contexts

--- contexts/AuthReducer.js
+++ contexts/AuthReducer.js
@@ -1,1 +1,28 @@
 import React from "react";
+
+// constants
+const LOGIN_USER = "LOGIN_USER";
+const LOGOUT_USER = "LOGOUT_USER";
+const REGISTER_USER = "REGISTER_USER";
+
+const initialState = {
+  isLoading: true,
+  userName: null,
+  userToken: null,
+};
+
+const loginReducer = (prevState, action) => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return {};
+
+    case LOGOUT_USER:
+      return {};
+
+    case REGISTER_USER:
+      return {};
+
+    default:
+      return prevState;
+  }
+};
