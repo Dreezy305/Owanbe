9b62b3597c476ca9bee33053d755b74c09320e12
Dreezy <bankoleidris@gmail.com>
2022-03-20 09:49:16 +0000

    drawer screen

--- navigations/Drawers.js
+++ navigations/Drawers.js
@@ -1,5 +1,9 @@
 import { View, Text } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import HomeScreen from "../screens/home";
 import React from "react";
 import Tabs from "./Tabs";
@@ -9,18 +13,36 @@ import PubAccessScreen from "../screens/PublicAccess";
 
 const Drawer = createDrawerNavigator();
 
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Help"
+        // onPress={() => Linking.openURL("https://mywebsite.com/help")}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
 export default function DrawerScreen() {
   return (
     <Drawer.Navigator>
       <Drawer.Screen
         name="HomeScreen"
         component={Tabs}
-        options={{ headerShown: false }}
+        options={{ headerShown: false, title: "Home" }}
       />
-      {/* <Drawer.Screen
-        name="Help"
-        component={<></>}
-      /> */}
+      <Drawer.Screen
+        name="My account"
+        component={MyAccScreen}
+        options={{ headerShown: false, title: "My account" }}
+      />
+      <Drawer.Screen
+        name="My favourites"
+        component={MyFavScreen}
+        options={{ headerShown: false, title: "My favourites" }}
+      />
     </Drawer.Navigator>
   );
 }
