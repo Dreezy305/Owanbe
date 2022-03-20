b45cde24de270b48ff2df83c6ad5d7d0d67a037e
Dreezy <bankoleidris@gmail.com>
2022-03-20 10:15:30 +0000

    drawer

--- navigations/Drawers.js
+++ navigations/Drawers.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -10,6 +10,13 @@ import Tabs from "./Tabs";
 import MyAccScreen from "../screens/MyAcc";
 import MyFavScreen from "../screens/MyFavs";
 import PubAccessScreen from "../screens/PublicAccess";
+import { icons } from "../constants";
+import {
+  TextAlignments,
+  COLORS,
+  FontWeights,
+  FontStyles,
+} from "../constants/color_theme_styles";
 
 const Drawer = createDrawerNavigator();
 
@@ -27,21 +34,119 @@ function CustomDrawerContent(props) {
 
 export default function DrawerScreen() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator screenOptions={{}}>
       <Drawer.Screen
         name="HomeScreen"
         component={Tabs}
-        options={{ headerShown: false, title: "Home" }}
+        options={{
+          headerShown: false,
+          title: "Home",
+          drawerIcon: ({ props }) => {
+            return (
+              <View>
+                <Image
+                  source={icons.home}
+                  resizeMethod="scale"
+                  resizeMode="contain"
+                  style={{ width: 20 }}
+                  {...props}
+                />
+              </View>
+            );
+          },
+          drawerLabelStyle: {
+            color: "rgba(0, 0, 0, 0.87)",
+            fontFamily: "Avenir_Black",
+            fontSize: 14,
+            fontWeight: FontWeights.fw800,
+            background: "rgba(226, 0, 0, 0.2)",
+          },
+        }}
       />
       <Drawer.Screen
         name="My account"
         component={MyAccScreen}
-        options={{ headerShown: false, title: "My account" }}
+        options={{
+          headerShown: false,
+          title: "My account",
+          drawerIcon: ({ props }) => {
+            return (
+              <View>
+                <Image
+                  source={icons.myAcc}
+                  resizeMethod="scale"
+                  resizeMode="contain"
+                  style={{ width: 20 }}
+                  {...props}
+                />
+              </View>
+            );
+          },
+          drawerLabelStyle: {
+            color: "rgba(0, 0, 0, 0.87)",
+            fontFamily: "Avenir_Black",
+            fontSize: 14,
+            fontWeight: FontWeights.fw800,
+            background: "rgba(226, 0, 0, 0.2)",
+          },
+        }}
       />
       <Drawer.Screen
         name="My favourites"
         component={MyFavScreen}
-        options={{ headerShown: false, title: "My favourites" }}
+        options={{
+          headerShown: false,
+          title: "My favourites",
+          drawerIcon: ({ props }) => {
+            return (
+              <View>
+                <Image
+                  source={icons.home}
+                  resizeMethod="scale"
+                  resizeMode="contain"
+                  style={{ width: 20 }}
+                  {...props}
+                />
+              </View>
+            );
+          },
+          drawerLabelStyle: {
+            color: "rgba(0, 0, 0, 0.87)",
+            fontFamily: "Avenir_Black",
+            fontSize: 14,
+            fontWeight: FontWeights.fw800,
+            background: "rgba(226, 0, 0, 0.2)",
+          },
+        }}
+      />
+
+      <Drawer.Screen
+        name="PubAccessScreen"
+        component={PubAccessScreen}
+        options={{
+          headerShown: false,
+          title: "Public Access",
+          drawerIcon: ({ props }) => {
+            return (
+              <View>
+                <Image
+                  source={icons.nearest}
+                  resizeMethod="scale"
+                  resizeMode="contain"
+                  {...props}
+                  style={{ width: 20 }}
+                />
+              </View>
+            );
+          },
+          drawerLabelStyle: {
+            color: "rgba(0, 0, 0, 0.87)",
+            fontFamily: "Avenir_Black",
+            fontSize: 14,
+            fontWeight: FontWeights.fw800,
+            background: "rgba(226, 0, 0, 0.2)",
+          },
+        }}
       />
     </Drawer.Navigator>
   );
